9a47c1ab2ddcac5545b9a03420030125427e541b
Carolina Gonzalez <rpcgonzalez@gmail.com>
2021-01-23 20:49:59 -0500

    Adding the footer

---
+++ contact-form.php
@@ -0,0 +1,21 @@
+<?php
+  echo '<p>Hello World</p>'; 
+// echo "<pre>";
+
+//     print_r($_POST);
+
+//     $userName = $_POST['name']; 
+//     $userPhone = $_POST['phone']; 
+//     $userEmail = $_POST['email']; 
+//     $userMessage = $_POST['message']; 
+
+//     $to = "carolinagonzalezcontact@gmail.com"
+//     $body = "";
+
+//     $body .= "From: ".$userName. "\r\n";
+//     $body .= "Email: ".$userEmail. "\r\n";
+//     $body .= "Phone: ".$userPhone. "\r\n";
+//     $body .= "Message: ".$message. "\r\n";
+
+// echo "</pre>";
+?>
